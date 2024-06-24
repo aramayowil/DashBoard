0e675b68cce55612dbeb8416e9f83422cc5509bf
David <aramayowil@gmail.com>
2024-06-24 11:48:06 -0300

    agrendoTabla

--- frontend/src/components/CardResume.jsx
+++ frontend/src/components/CardResume.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import {
   Button,
   Divider,
@@ -8,83 +8,92 @@ import {
   Card,
   ConfigProvider,
   Select,
+  Input,
   Form,
-} from "antd"
+} from "antd";
 
-const { Title } = Typography
-const { Item } = Form
+const { Title } = Typography;
 
 function CardResume() {
   return (
     <>
-      <Form vertical>
-        <Item label='Forma de pago' name='formaPago'>
-          <Select
-            defaultValue='efectivo'
-            style={{ width: "100%" }}
-            options={[
-              { value: "efectivo", label: "Efectivo" },
-              { value: "tarjeta", label: "Tarjeta" },
-            ]}
-          />
-        </Item>
-      </Form>
-    </>
-  )
-}
-
-export default CardResume
-
-{
-  /* <ConfigProvider
-theme={{
-  components: {
-    Card: {},
-  },
-}}
->
-<Card
-  title='Resumen de venta'
-  bordered={true}
-  style={{
-    width: 400,
-  }}
->
-  <Row justify={"start"}>
-    <Col xs={24} md={24} lg={24} xl={24}>
-      <div style={{ marginBottom: 8 }}>
-        <Title style={{ fontSize: 14 }}> Forma de pago</Title>
-        <Select
-          defaultValue='efectivo'
+      <ConfigProvider
+        theme={{
+          components: {
+            Card: {},
+          },
+        }}
+      >
+        <Card
+          title="Resumen de venta"
+          bordered={true}
           style={{ width: "100%" }}
-          options={[
-            { value: "efectivo", label: "Efectivo" },
-            { value: "tarjeta", label: "Tarjeta" },
-          ]}
-        />
-        <Divider />
-      </div>
-    </Col>
-  </Row>
-  <Row justify={"space-between"} style={{ marginBottom: 8 }}>
-    <Col>Valor de la compra</Col>
-    <Col>$297.350,48</Col>
-  </Row>
-  <Row justify={"space-between"} style={{ marginBottom: 8 }}>
-    <Col>Descuentos</Col>
-    <Col>-$0</Col>
-  </Row>
-  <Row justify={"space-between"} style={{ marginBottom: 8 }}>
-    <Col>Subtotal</Col>
-    <Col>$297.350,48</Col>
-  </Row>
-  <Row justify={"center"} style={{ textAlign: "center" }}>
-    <Col xs={24} md={24} lg={24} xl={24}>
-      <Title level={4}>Total: $ 297.350,48</Title>
-      <Divider />
-      <Button style={{ width: "100%" }}>FINALIZAR COMPRA</Button>
-    </Col>
-  </Row>
-</Card>
-</ConfigProvider> */
+        >
+          <Row justify={"start"}>
+            <Col xs={24} md={24} lg={24} xl={24}>
+              <div style={{ marginBottom: 8 }}>
+                <Title style={{ fontSize: 14 }}> Forma de pago</Title>
+                <Select
+                  defaultValue="efectivo"
+                  style={{ width: "100%" }}
+                  options={[
+                    { value: "efectivo", label: "Efectivo" },
+                    { value: "tarjeta", label: "Tarjeta" },
+                  ]}
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row justify={"start"} style={{ gap: 9 }}>
+            <Col span={5}>
+              <Typography.Title style={{ fontSize: 14 }}>
+                Serie
+              </Typography.Title>
+              <Input variant="filled" disabled defaultValue="001"></Input>
+            </Col>
+            <Col span={12}>
+              <Typography.Title style={{ fontSize: 14 }}>
+                Nro Factura
+              </Typography.Title>
+              <Input variant="filled" disabled defaultValue="00001231"></Input>
+            </Col>
+          </Row>
+
+          <Divider />
+
+          <Row style={{ marginBottom: 8 }}>
+            <Col span={12}>Valor de la compra</Col>
+            <Col span={12} style={{ textAlign: "end" }}>
+              $297.350,48
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: 8 }}>
+            <Col span={12}>Descuentos</Col>
+            <Col span={12} style={{ textAlign: "end" }}>
+              -$0
+            </Col>
+          </Row>
+
+          <Row style={{ marginBottom: 8 }}>
+            <Col span={12}>Subtotal</Col>
+            <Col span={12} style={{ textAlign: "end" }}>
+              $297.350,48
+            </Col>
+          </Row>
+
+          <Row justify={"center"} style={{ textAlign: "center" }}>
+            <Col xs={24} md={24} lg={24} xl={24}>
+              <Title level={4}>Total: $ 297.350,48</Title>
+              <Divider />
+              <Button type="primary" style={{ width: "100%" }}>
+                FINALIZAR COMPRA
+              </Button>
+            </Col>
+          </Row>
+        </Card>
+      </ConfigProvider>
+    </>
+  );
 }
+
+export default CardResume;
